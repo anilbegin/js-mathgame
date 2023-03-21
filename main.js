5f44d6a3824ee5d59d6c7ef7b786f4c0eee1dad1
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-03-21 15:51:45 +0530

    progress bar render and reset according to the score

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const ourForm = document.querySelector(".our-form")
 const ourField = document.querySelector(".our-field")
 const pointsNeeded = document.querySelector(".points-needed")
 const mistakesAllowed = document.querySelector(".mistakes-allowed")
+const progressBar = document.querySelector(".progress-inner")
 let state = {
   score: 0,
   wrongAnswers: 0
@@ -46,6 +47,7 @@ function handleSubmit(e) {
   if (parseInt(ourField.value, 10) === correctAnswer) {
     state.score++
     pointsNeeded.textContent = 10 - state.score
+    renderProgressBar()
     updateProblem()
   } else {
     state.wrongAnswers++
@@ -57,8 +59,13 @@ function handleSubmit(e) {
 function checkLogic() {
   // if you won
   if (state.score === 10) {
-    alert("Congrats, you won.")
-    resetGame()
+    // the timeout helps in maintaining expected execution..
+    // ..without the timeout the alert pops up before rendering the progressbar
+    // ..and the game resets before clicking on 'OK button' in the alert pop-up
+    setTimeout(() => {
+      alert("Congrats, you won.")
+      resetGame()
+    }, 200)
   }
 
   // if you lost
@@ -74,4 +81,9 @@ function resetGame() {
   state.wrongAnswers = 0
   pointsNeeded.textContent = 10
   mistakesAllowed.textContent = 2
+  renderProgressBar()
+}
+
+function renderProgressBar() {
+  progressBar.style.transform = `scaleX(${state.score / 10})`
 }
